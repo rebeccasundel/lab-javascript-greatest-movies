4fbe643229668ad41c0e8236f20c9c99b4919cd1
Rebecca Sundel <sundelrp@gmail.com>
2021-06-26 17:39:02 -0400

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,59 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(moviesArray) {
+  return moviesArray.map(movie => {
+    return movie.director;
+  });
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(moviesArray) {
+  return moviesArray.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length;
+   {
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(moviesArray) {
+  return moviesArray.length > 0 ? Number((moviesArray.reduce((acc, current) => acc += current.score ? current.score : 0, 0) / movies.array.length).toFixed(2)) : 0;
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
 
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(moviesArray) {
+   return moviesArray.map(movie => movie).sort((a, b) => {
+    if (a.year < b.year) {
+      return -1;
+    } else if (a.year > b.year) {
+      return 1;
+    } else if (a.title > b.title) {
+      return -1;
+       } else if (a.title > b.title) {
+      return 1;
+    } else {
+      return 0;
+    }
+    });
+
+    }
+   
+    return sortedArray;
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(moviesArray) {
+  const alphabeticalOrder = arr.filter(movie => {
+    return movie.title;
+  })
+  alphabeticalOrder.sort();
+  alphabeticalOrder.length = 20;
+  const turnedString = Object.values(alphabeticalOrder);
+  return turnedString;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
