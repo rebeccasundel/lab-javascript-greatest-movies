de96333d303ab38b9a0660af0c5ddb72dd164440
Rebecca Sundel <sundelrp@gmail.com>
2021-07-01 16:16:38 -0400

    done

--- src/movies.js
+++ src/movies.js
@@ -62,18 +62,3 @@ function turnHoursToMinutes() {}
 function bestYearAvg() {}
 
 
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    howManyMovies,
-    scoresAverage,
-    dramaMoviesScore,
-    orderByYear,
-    orderAlphabetically,
-    turnHoursToMinutes,
-    bestYearAvg,
-  };
-}
